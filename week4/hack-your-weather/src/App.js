2c3a225eeeaddfad5dfeac4e1e4996bb223798f8
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-12-01 21:26:07 +0100

    Update App.js

--- week4/hack-your-weather/src/App.js
+++ week4/hack-your-weather/src/App.js
@@ -7,22 +7,6 @@ function App() {
   return (
     <div className="App">
       <Routes />
-      {/* <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home" exact>
-            <Home />
-          </Route>
-          <Route path="/home/:cityId">
-            <ChartPage />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router> */}
     </div>
   );
 }
